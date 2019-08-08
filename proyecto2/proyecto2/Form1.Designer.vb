39b5a0023bf3150647cca4cf76886e44c1fe0263
VascoSilver <hornosvalentin@gmail.com>
2019-08-08 08:27:00 -0300

    Se modifico el formulario y se creo un login

--- proyecto2/proyecto2/Form1.Designer.vb
+++ proyecto2/proyecto2/Form1.Designer.vb
@@ -22,9 +22,93 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblUsuario = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txbUsuario = New System.Windows.Forms.TextBox()
+        Me.txbContraseña = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUsuario
+        '
+        Me.lblUsuario.AutoSize = True
+        Me.lblUsuario.Font = New System.Drawing.Font("Carlito", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsuario.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblUsuario.Location = New System.Drawing.Point(132, 83)
+        Me.lblUsuario.Name = "lblUsuario"
+        Me.lblUsuario.Size = New System.Drawing.Size(147, 39)
+        Me.lblUsuario.TabIndex = 0
+        Me.lblUsuario.Text = "USUARIO:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Carlito", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label1.Location = New System.Drawing.Point(109, 148)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 39)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CONTRASEÑA:"
+        '
+        'txbUsuario
+        '
+        Me.txbUsuario.Location = New System.Drawing.Point(323, 99)
+        Me.txbUsuario.Name = "txbUsuario"
+        Me.txbUsuario.Size = New System.Drawing.Size(313, 20)
+        Me.txbUsuario.TabIndex = 2
+        '
+        'txbContraseña
+        '
+        Me.txbContraseña.Location = New System.Drawing.Point(323, 164)
+        Me.txbContraseña.Name = "txbContraseña"
+        Me.txbContraseña.Size = New System.Drawing.Size(313, 20)
+        Me.txbContraseña.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Carlito", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(220, 263)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "REGISTRAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Carlito", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(392, 263)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 37)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "INGRESAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(692, 357)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txbContraseña)
+        Me.Controls.Add(Me.txbUsuario)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblUsuario)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents lblUsuario As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txbUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txbContraseña As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- proyecto2/proyecto2/Form1.vb
+++ proyecto2/proyecto2/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblUsuario_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblUsuario.Click
+
+    End Sub
 End Class
